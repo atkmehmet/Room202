e5817a13387f144dcce5d419a5d83141e9744195
atkmehmet <durmazmehmet47@gmail.com>
2025-01-04 00:41:03 +0300

    first examples insert in database

--- app/src/main/java/com/example/records/MainScreen.kt
+++ app/src/main/java/com/example/records/MainScreen.kt
@@ -1,18 +1,22 @@
 package com.example.records
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun MainScreen(mainView: MainView = hiltViewModel()){
-    LaunchedEffect(Unit) {
+    val masters by mainView.masters.collectAsState()
 
-    }
-    Column {
-        Text(text = "Insert successful!", style = MaterialTheme.typography.bodyLarge)
+    LazyColumn {
+        items(masters.size) { it ->
+            Text("${masters[it].name}: ${masters[it].text}")
+        }
     }
 }
--- app/src/main/java/com/example/records/MainView.kt
+++ app/src/main/java/com/example/records/MainView.kt
@@ -3,15 +3,19 @@ package com.example.records
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainView @Inject constructor(private val dao: AppDao):ViewModel() {
-
+    private val _masters = MutableStateFlow<List<MasterTable>>(emptyList())
+    val masters: StateFlow<List<MasterTable>> = _masters
     init {
         viewModelScope.launch {
-            dao.insertMaster(MasterTable(1,"xx","xx Explain"))
+            dao.insertMaster(MasterTable(2,"xx","xx Explain"))
+            _masters.value = dao.getAllMasters() // Example query
         }
     }
 
