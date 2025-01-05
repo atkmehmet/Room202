6e1658e654b0e9eb6203ec50235088010ccd23cf
atkmehmet <durmazmehmet47@gmail.com>
2025-01-05 23:42:10 +0300

    second screen viewmodel

--- app/src/main/java/com/example/records/SubtableView.kt
+++ app/src/main/java/com/example/records/SubtableView.kt
@@ -4,15 +4,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.room.Dao
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SubtableView @Inject constructor(private val dao: Dao) :ViewModel() {
+class SubtableView @Inject constructor(private val dao: AppDao) :ViewModel() {
 
     var masterId by mutableStateOf("")
     private set
@@ -21,5 +23,21 @@ class SubtableView @Inject constructor(private val dao: Dao) :ViewModel() {
 
     private val _subTables = MutableStateFlow<List<SubTable>>(emptyList())
     val subTables : StateFlow<List<SubTable>> = _subTables
-    
+
+    init {
+
+    }
+
+    fun updateMasterId(newMasterId:String){
+        masterId = newMasterId
+    }
+
+    fun updateText(newText:String){
+        text = newText
+    }
+    fun loadSubTable(){
+        viewModelScope.launch {
+            _subTables.value = dao.getSubsByMasterId(masterId = masterId.toInt())
+        }
+    }
 }
